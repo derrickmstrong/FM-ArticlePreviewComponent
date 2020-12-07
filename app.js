6daa817a0ab7130f4462063c949990c78a102698
Derrick Strong <derrickmstrong@gmail.com>
2020-12-07 10:02:14 -0600

    Updated social layout

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ share.addEventListener("click", () => {
       shareSocial.style.visibility = 'hidden';
       share.style.backgroundColor = 'hsl(210, 46%, 95%)';
     } else {
-      shareSocial.style.display = 'grid';
       shareSocial.style.visibility = 'visible';
       share.style.backgroundColor = 'hsl(217, 19%, 35%)';
     }
